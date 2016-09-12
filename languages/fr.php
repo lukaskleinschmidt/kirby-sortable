060f14dca8e91dafe9ace7a5cca1523198b01235
Mathieu Etienne <thiousi@gmail.com>
2016-09-12 19:15:08 +0200

    Create fr.php

---
+++ languages/fr.php
@@ -0,0 +1,16 @@
+<?php
+l::set(array(
+  'fields.modules.add'                => 'Ajouter',
+  'fields.modules.visible'            => 'Modules visibles',
+  'fields.modules.visible.info'       => 'Glisser un module invisible ici pour l'ordonner/le rendre visible.',
+  'fields.modules.invisible'          => 'Modules invisibles',
+  'fields.modules.invisible.info'     => 'Glisser un module visible ici pour le désordonner/le rendre invisible.',
+  'fields.modules.edit'               => 'Editer',
+  'fields.modules.delete'             => 'Supprimer',
+  'fields.modules.add.title.label'    => 'Ajouter un nouveau module',
+  'fields.modules.add.url.label'      => 'URL-appendix',
+  'fields.modules.add.template.label' => 'Template',
+  'fields.modules.delete.headline'    => 'Voulez-vous vraiment supprimer ce module ?',
+  'fields.modules.empty'              => 'Pas encore de modules.',
+  'fields.modules.add.first'          => 'Ajouter le premier module',
+));
