9fcd782d32b424ffdeeafbb74844e22ef96a02e0
Lukas Kleinschmidt <l.kleinschmidt@crolla-lowis.de>
2016-10-26 16:23:29 +0200

    cleanup

--- template.php
+++ template.php
@@ -2,11 +2,9 @@
   <?php foreach($field->modules() as $module): ?>
     <div class="module" data-visible="<?php echo $module->isVisible() ? 'true' : 'false'; ?>" data-uid="<?php echo $module->uid(); ?>">
       <?php if(true): ?>
-        <div class="module__preview">
-
+        <!-- <div class="module__preview">
           <?php echo $field->url('sort'); ?>
-
-        </div>
+        </div> -->
       <?php endif; ?>
       <nav class="module__navigation">
         <div class="module__title">
@@ -17,7 +15,8 @@
           <?php echo $module->title(); ?>
         </div>
         <a class="module__button" href="<?php echo $module->url('edit'); ?>" title="Edit"><i class="icon icon-left fa fa-pencil"></i> Edit</a>
-        <a class="module__button" href="" title="Delete"><i class="icon icon-left fa fa-trash-o"></i> Delete</a>
+        <!-- <a class="module__button" href="" title="Delete"><i class="icon icon-left fa fa-trash-o"></i> Delete</a> -->
+        <!-- <button class="module__button"><i class="icon fa fa-copy"></i></button> -->
         <?php if($module->isVisible()): ?>
           <button class="module__button" data-hide title="Hide"><i class="icon fa fa-toggle-on"></i></button>
         <?php else: ?>
