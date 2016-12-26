6d700db8c3ee730edbb604b8020f38fddb761cfd
Lukas Kleinschmidt <Lukas Kleinschmidt>
2016-12-26 21:21:40 +0100

    Fixes #18

--- fields/modules/modules.php
+++ fields/modules/modules.php
@@ -235,7 +235,7 @@ class ModulesField extends InputField {
     });
 
     // Sort modules
-    if($this->value()) {
+    if($modules->count() && $this->value()) {
       $i = 0;
 
       $order = a::merge(array_flip($this->value()), array_flip($modules->pluck('uid')));
