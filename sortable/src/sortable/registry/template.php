607f69a634eeabd81a885cdaac4dd5231e4969fa
Lukas Kleinschmidt <l.kleinschmidt@crolla-lowis.de>
2017-01-23 11:14:55 +0100

    update

---
+++ sortable/src/sortable/registry/template.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kirby\Sortable\Registry;
+
+use Exception;
+use Kirby;
+use A;
+
+class Template extends Kirby\Registry\Entry {
+
+  // Store of registered templates
+	protected static $templates = [];
+
+	/**
+	 * Adds a new template to the registry
+	 *
+	 * @param mixed $name
+	 * @param string $path
+	 */
+	public function set($name, $path = null) {
+
+    if(!$this->kirby->option('debug') || is_file($path)) {
+      return static::$templates[$name] = $path;
+    }
+
+    throw new Exception('The template does not exist at the specified path: ' . $path);
+
+	}
+
+  /**
+	 * Retreives a registered template file
+	 * If called without params, retrieves all registered templates
+	 *
+	 * @param  string $name
+	 * @return mixed
+	 */
+  public function get($name = null) {
+
+    if(is_null($name)) {
+      return static::$templates;
+    }
+
+    return a::get(static::$templates, $name);
+
+  }
+
+}
---
+++ sortable/src/sortable/roots.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kirby\Sortable;
+
+use Obj;
+
+class Roots extends Obj {
+
+  public $index;
+
+  public function __construct($root) {
+
+    $this->index = $root;
+
+    $this->translations = $this->index . DS . 'translations';
+    $this->templates    = $this->index . DS . 'templates';
+    $this->layouts      = $this->index . DS . 'layouts';
+    $this->actions      = $this->index . DS . 'actions';
+
+  }
+
+  public function __debuginfo() {
+    return parent::__debuginfo();
+  }
+
+}
